82777ed4691bd2cee550ef689ed3ac65b4ee16dc
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 08:10:00 -0700

    Made files executable

--- binary_trees.h
+++ binary_trees.h
@@ -50,7 +50,7 @@ int binary_tree_is_root(const binary_tree_t *nodes);
 void binary_tree_preorder(const binary_tree_t *tree, void (*funcs)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*funcs)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*funcs)(int));
-size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *trees);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
