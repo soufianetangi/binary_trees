ce4505a74e52cfeb919f438e37bdf122f521f037
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 08:31:27 -0700

    Made files executable

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: 0 if tree is NULL.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t lefs = 0, rigs = 0;
+
+		lefs = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rigs = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+
+		return ((lefs > rigs) ? lefs : rigs);
+	}
+
+	return (0);
+}
