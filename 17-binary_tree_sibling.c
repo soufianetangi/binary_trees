06b8db5399a060f08592cb9116a4ac5d6dffcac6
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 09:16:14 -0700

    Made files executable

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,16 +3,16 @@
 /**
  * binary_tree_sibling - Finds the sibling of a node.
  *
- * @node: A pointer to the node to find the sibling.
+ * @nodee: A pointer to the node to find the sibling.
  *
  * Return: NULL if node is NULL or parent is NULL or node has no sibiling.
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *nodee)
 {
-	if (node == NULL || node->parents == NULL)
+	if (nodee == NULL || nodee->parent == NULL)
 		return (NULL);
-	if (node->parents->left == node)
-		return (node->parents->right);
+	if (nodee->parent->left == nodee)
+		return (nodee->parent->right);
 
-	return (node->parents->left);
+	return (nodee->parent->left);
 }
