f6b91ef8409c00dc9ad9fbfdcfe9704e3dc02d9f
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 08:26:23 -0700

    Made files executable

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,19 +3,19 @@
 /**
  * binary_tree_leaves - Counts the leaves in a binary tree.
  *
- * @trees: A pointer to the root node of the tree to count the number of leaves.
+ * @tree: A pointer to the root node of the tree to count the number of leaves.
  *
  * Return: 0 if tree is NULL.
  */
-size_t binary_tree_leaves(const binary_tree_t *trees)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+	size_t leavess = 0;
 
-	if (trees)
+	if (tree)
 	{
-		leaves += (!trees->left && !trees->right) ? 1 : 0;
-		leaves += binary_tree_leaves(trees->left);
-		leaves += binary_tree_leaves(trees->right);
+		leavess += (!tree->left && !tree->right) ? 1 : 0;
+		leavess += binary_tree_leaves(tree->left);
+		leavess += binary_tree_leaves(tree->right);
 	}
 
 	return (leaves);
