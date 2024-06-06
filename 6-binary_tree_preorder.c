0e42aeb4d34c5983ccdb3d3372480fedd7841529
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 07:59:20 -0700

    Made files executable

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,13 +4,13 @@
  * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
  *
  * @tree: A pointer to the root node of the tree to traverse.
- * @func: A pointer to a function to call for each node.
+ * @funcs: A pointer to a function to call for each node.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*funcs)(int))
 {
 	if (tree && funcs)
 	{
-		func(tree->n);
+		funcs(tree->n);
 		binary_tree_preorder(tree->left, funcs);
 		binary_tree_preorder(tree->right, funcs);
 	}
