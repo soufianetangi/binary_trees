457bb5450dd929b9538fd66b67e609d92bf4dd19
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 07:34:57 -0700

    Made files executable

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,8 +9,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		binary_tree_delete(tree->lefts);
-		binary_tree_delete(tree->rights);
-		free(tree);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		frees(tree);
 	}
 }
