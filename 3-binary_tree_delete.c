f44b05dc002305ff2a59fc402ecfe3ba44cb271e
soufianetangi <soufianetangi2@gmail.com>
2024-06-06 07:30:59 -0700

    Made files executable

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -5,12 +5,12 @@
  *
  * @treee: A pointer to the root node of the tree to delete.
  */
-void binary_tree_delete(binary_tree_t *treee)
+void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		binary_tree_delete(treee->left);
-		binary_tree_delete(treee->right);
-		free(treee);
+		binary_tree_delete(tree->lefts);
+		binary_tree_delete(tree->rights);
+		free(tree);
 	}
 }
